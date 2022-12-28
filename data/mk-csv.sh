5c17530e8c81968cf63bee9f51730193956b2f50
Alex Ionescu <aaionescu@protonmail.com>
2022-12-29 00:54:46 +0200

    Escape CSV fields; Download missing samples

--- data/mk-csv.sh
+++ data/mk-csv.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 set -euo pipefail
 
-URI="https://accent.gmu.edu/browse_language.php?function=detail&speakerid="
-DEST="${1:-html}"
+SRC="${1:-html}"
 
-echo "speaker_id;file_name;birth_place;native_langs;other_langs;age;sex;age_of_english_onset;english_learning_method;english_residence;years_of_english_residence"
+printf "speaker_id;file_name;sample_name;birth_place;native_langs;other_langs;age;sex;age_of_english_onset;english_learning_method;english_residence;years_of_english_residence"
 
 for i in $(seq 1 3007); do
-  echo -n "$i;"
+  printf "\n$i;"
 
-  cat "$DEST/$i.html" |
-  pup 'h5 em, .bio li json{}' |
-  jq -r '.[]|(.children|.[]?.text),.text|select(.!=null and (startswith("(")|not))' |
-  sed -r 's/(.*:$)/;/' |
-  sed -r 's/([0-9]+), /\1;/' |
+  cat "$SRC/$i.html" |
+  pup 'source, h5 em, .bio li json{}' |
+  jq '.[]|.src,(.children|.[]?.text),.text|select(.!=null and (startswith("(")|not))' |
+  sed -r 's/.*:"$/;/' |
+  sed -r 's/([0-9]+), /\1";"/' |
+  sed -r 's/\/soundtracks\/(.+)\.mp3"/\1";/' |
   sed -r 's/ years//' |
   tr -d '\n'
-
-  echo
-done
+done |
+sed -r '/\/soundtracks\//d'

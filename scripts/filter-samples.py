cd3a33e540818fb2ec7a3c5361f68f8f6de7d6fc
Alex Ionescu <aaionescu@protonmail.com>
2023-01-27 17:21:48 +0100

    Reindex primary and secondary labels

--- scripts/filter-samples.py
+++ scripts/filter-samples.py
@@ -8,6 +8,14 @@ def with_suffix(path, suffix):
   p = Path(path)
   return str(p.with_stem(p.stem + suffix))
 
+def reindex(rows, field_idx):
+  unique = list(set(map(lambda row: int(row[field_idx]), rows)))
+  unique.sort()
+  dict = { str(f): str(i) for i, f in enumerate(unique) }
+
+  for row in rows:
+    row[field_idx] = dict[row[field_idx]]
+
 def main():
   if len(argv) < 3:
     print("Usage: filter-samples.py <csv_file> <min_samples>")
@@ -17,26 +25,29 @@ def main():
   min_samples = int(argv[2])
 
   with open(csv_file, "r") as f:
-    header = f.readline()
-    rows = [row.split(";") for row in f.readlines()]
+    header = f.readline().strip()
+    header_cols = header.split(";")
+    rows = [[col.strip() for col in row.split(";")] for row in f.readlines()]
 
-  label_idx = header.split(";").index("class_labels")
-  next_label = 0
+  label_idx = header_cols.index("class_labels")
+  primary_idx = header_cols.index("primary_lang_group_id")
+  secondary_idx = header_cols.index("secondary_lang_group_id")
+
+  filtered = []
+  for _, g in groupby(rows, lambda row: row[label_idx]):
+    rows = list(g)
+    if len(rows) >= min_samples:
+      filtered.extend(rows)
 
   with open(with_suffix(csv_file, str(min_samples)), "w") as new_csv:
     new_csv.write(header)
 
-    for _, g in groupby(rows, lambda row: row[label_idx]):
-      rows = list(g)
+    reindex(filtered, label_idx)
+    reindex(filtered, primary_idx)
+    reindex(filtered, secondary_idx)
 
-      if len(rows) < min_samples:
-        continue
-
-      for row in rows:
-        row[label_idx] = str(next_label)
-        new_csv.write(";".join(row))
-
-      next_label += 1
+    for row in filtered:
+      new_csv.write(";".join(row) + "\n")
 
 if __name__ == "__main__":
   main()
